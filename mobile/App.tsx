305c05d9667da52756449269785b6494d3d36b33
Luiz Rioja <lulyrioja@gmail.com>
2022-09-13 18:07:36 -0300

    feat: load custom font and style statusBar

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,5 +1,33 @@
-import { Text } from "react-native";
+import {
+  Inter_400Regular,
+  Inter_600SemiBold,
+  Inter_700Bold,
+  Inter_900Black,
+  useFonts,
+} from "@expo-google-fonts/inter";
+import { StatusBar, View } from "react-native";
+
+import { Background } from "./src/components/Background";
+import { Loading } from "./src/components/Loading";
+import { Home } from "./src/screens/Home";
 
 export default function App() {
-  return <Text>Open up App.tsx to start working on your app!</Text>;
+  const [fontsIsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_600SemiBold,
+    Inter_700Bold,
+    Inter_900Black,
+  });
+
+  return (
+    <Background>
+      <StatusBar
+        barStyle={"light-content"}
+        backgroundColor="transparent"
+        translucent
+      />
+
+      {fontsIsLoaded ? <Home /> : <Loading />}
+    </Background>
+  );
 }
